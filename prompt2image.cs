849d64a04def1ef5d0047679ba0414d1a9426d14
farizhirzan <129821528+farizhirzan@users.noreply.github.com>
2025-01-04 11:07:14 +0700

    Update prompt2image.cs

--- prompt2image.cs
+++ prompt2image.cs
@@ -53,7 +53,7 @@ public class prompt2image : MonoBehaviour
     public async void Start()
     {
         var client = new HttpClient();
-        client.DefaultRequestHeaders.Add("Authorization", "Token r8_0D3MzXy3oqHGCIYkO0VjdBCz8VFFPMN29wcjq");
+        client.DefaultRequestHeaders.Add("Authorization", "");
 
         var request = new HttpRequestMessage(HttpMethod.Post, "https://api.replicate.com/v1/predictions");
         var content = new StringContent(JsonConvert.SerializeObject(new
@@ -100,7 +100,7 @@ public class prompt2image : MonoBehaviour
             await Task.Delay(1000); // Poll every 1 second
 
             var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("Authorization", "Token r8_0D3MzXy3oqHGCIYkO0VjdBCz8VFFPMN29wcjq");
+            client.DefaultRequestHeaders.Add("Authorization", "");
 
             var request = new HttpRequestMessage(HttpMethod.Get, "https://api.replicate.com/v1/predictions/" + idrequest);
 
